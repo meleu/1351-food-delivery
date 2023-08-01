7b7643a201012fc7e67482fbad363d5e1dce3d63
Augusto Lopes <meleu.dev@gmail.com>
2023-08-01 20:13:06 -0300

    make rake happy

--- app/views/orders_view.rb
+++ app/views/orders_view.rb
@@ -1,20 +1,18 @@
 class OrdersView
   def display_orders(orders)
     if orders.empty?
-      puts "No orders to be displayed..."
+      puts 'No orders to be displayed...'
       return false
     end
 
-    puts "List of orders:"
     orders.each_with_index do |order, index|
       puts "#{index + 1}. '#{order.employee.username}' must deliver '#{order.meal.name}' to '#{order.customer.name}'"
     end
-    puts
   end
 
   def ask_user_for_index
-    puts "index?"
-    print "> "
+    puts 'index?'
+    print '> '
     gets.chomp.to_i - 1
   end
 end
